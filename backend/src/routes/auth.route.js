f73fc06f08f5ee838d6089a664cbdbf18f922ff5
Prerak siddhpura <prerak.siddhpura@gmail.com>
2025-04-18 16:14:37 +0530

    .

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,17 +1,17 @@
 import express from 'express';
+import { checkAuth, login, logout, signup, updateProfile } from '../controllers/auth.controller.js';
+import { protectRoute } from '../middleware/auth.middleware.js';
 
 const router = express.Router();
 
-router.post("/signup", (req, res) =>{
-    res.send("signup route")
-})
+router.post("/signup", signup)
 
-router.post("/login", (req, res) =>{
-    res.send("login route")
-})
+router.post("/login", login)
 
-router.post("/logout", (req, res) =>{
-    res.send("logout route")
-})
+router.post("/logout", logout)
+
+router.put("/update-profile", protectRoute, updateProfile)
+
+router.get("/check" , protectRoute, checkAuth) 
 
 export default router;
