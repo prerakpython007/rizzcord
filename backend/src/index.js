2f591e9d03f4850bf35359a8fa0deeb450789234
Prerak siddhpura <prerak.siddhpura@gmail.com>
2025-04-26 00:39:07 +0530

    .

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
+import cors from 'cors';
 import { connectDB } from './lib/db.js';
 import cookieParser from 'cookie-parser';
 
@@ -12,6 +13,11 @@ import cookieParser from 'cookie-parser';
 
  app.use(express.json());
  app.use(cookieParser());
+ app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+ })
+);
 
  app.use("/api/auth" , authRoutes)
  app.use("/api/message" , messageRoutes)
---
+++ frontend/src/store/useAuthStore.js
@@ -0,0 +1,40 @@
+import {create} from 'zustand';
+import {axiosInstance} from '../lib/axios.js';
+import {toast} from 'react-hot-toast';
+
+export const useAuthStore = create((set) =>({
+    authUser: null,
+    isSigningUp: false,
+    isLoggingIn: false,
+    isUpdatingProfile: false,
+    isCheckingAuth: true,
+
+    checkAuth: async () => {
+        try{
+            const res = await axiosInstance.get("/auth/check")
+
+            set({authUser: res.data});
+
+        }catch (error) {
+            console.log("Error checkAuth:", error);
+            set({authUser: null});
+        }finally{
+            set({isCheckingAuth: false});
+        }
+    },
+
+    signup: async (data) => {
+        set({isSigningUp: true});
+        try{
+            const res = await axiosInstance.post("/auth/signup", data);
+            set({authUser: res.data});
+            toast.success("Account created successfully!");
+
+        }catch (error) {
+            toast.error(error.response.data.message)
+            console.log("Error signup:", error);
+        }finally{
+            set({isSigningUp: false});
+        }
+    }
+}))
